f77007dc95ba6cf68fcd0c2d345493181c2dc769
petcupaula <petcu.paula@gmail.com>
2021-02-14 14:38:48 +0100

    Average over several ph meter value samples

--- arduino_ph_meter/arduino_ph_meter.ino
+++ arduino_ph_meter/arduino_ph_meter.ino
@@ -1,9 +1,10 @@
 int phpin_value; 
 int vplus = 5; //5V provided to the module
+int samples = 10;
 
 // Derived from ph_calibration/ph_calibration.org
-float ph_offset = 20.61
-float ph_coefficient = -5.23
+float ph_offset = 20.61;
+float ph_coefficient = -5.23;
 
 void setup() 
 { 
@@ -18,13 +19,18 @@ float voltage(int val) {
   return val * vplus / 1024.0; 
 }
 
-void loop() 
-{ 
-  phpin_value = analogRead(A0); 
+void loop() { 
+
+  float samples_sum = 0;
+  for(int i=0; i<samples; i++) {
+    samples_sum += analogRead(A0); 
+    delay(10);
+    }
+  phpin_value = samples_sum/samples;
+  
   Serial.print("V: ");
   Serial.print(voltage(phpin_value));
-  //Serial.print(" pH: ");
   Serial.print(" pH: ");
   Serial.println(ph(phpin_value));
-  delay(500); 
+  delay(500);
 }
