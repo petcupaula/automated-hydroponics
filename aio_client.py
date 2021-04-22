946e1b689a3e7fdbcfaafbbca8ff9c33f90ec191
petcupaula <petcu.paula@gmail.com>
2021-04-22 22:40:25 +0200

    Add adafruit io client to subscribe to feed changes

---
+++ aio_client.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import sys
+import serial
+from Adafruit_IO import MQTTClient
+import config
+
+ADAFRUIT_IO_KEY = config.ADAFRUIT_IO_KEY
+ADAFRUIT_IO_USERNAME = config.ADAFRUIT_IO_USERNAME
+FEED_ID = 'hydroponics'
+
+def connected(client):
+    print('Connected to Adafruit IO!  Listening for {0} changes...'.format(FEED_ID))
+    client.subscribe(FEED_ID)
+
+def subscribe(client, userdata, mid, granted_qos):
+    print('Subscribed to {0} with QoS {1}'.format(FEED_ID, granted_qos[0]))
+
+def disconnected(client):
+    print('Disconnected from Adafruit IO!')
+    sys.exit(1)
+
+def message(client, feed_id, payload):
+    print('Feed {0} received new value: {1}'.format(feed_id, payload))
+    ser.write((payload+"\n").encode('ascii'))
+
+if __name__ == '__main__':
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser.flush()
+
+    client = MQTTClient(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+
+    client.on_connect    = connected
+    client.on_disconnect = disconnected
+    client.on_message    = message
+    client.on_subscribe  = subscribe
+
+    client.connect()
+
+    client.loop_blocking()
--- config_template.py
+++ config_template.py
@@ -2,3 +2,5 @@ url = "192.168.0.1"
 ca = "/home/pi/ca.pem"
 cert = "/home/pi/client.pem"
 private = "/home/pi/client.key"
+ADAFRUIT_IO_KEY = 'iokeyfromadafruitio'
+ADAFRUIT_IO_USERNAME = 'usernamefromadafruitio'
