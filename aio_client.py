212b6fb441f821157154ebf34988b9e6f7198a00
petcupaula <petcu.paula@gmail.com>
2021-05-16 14:39:45 +0200

    Send sensor data to Adafruit IO

--- aio_client.py
+++ aio_client.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+import time
 import glob
 import serial
 from Adafruit_IO import MQTTClient
@@ -7,14 +8,17 @@ import config
 
 ADAFRUIT_IO_KEY = config.ADAFRUIT_IO_KEY
 ADAFRUIT_IO_USERNAME = config.ADAFRUIT_IO_USERNAME
-FEED_ID = 'hydroponics'
+FEED_ID_CMDS = 'hydroponics'
+FEED_ID_WATERTEMP = 'hydro-watertemp'
+FEED_ID_PH = 'hydro-ph'
+FEED_ID_TDS = 'hydro-tds'
 
 def connected(client):
-    print('Connected to Adafruit IO!  Listening for {0} changes...'.format(FEED_ID))
-    client.subscribe(FEED_ID)
+    print('Connected to Adafruit IO!  Listening for {0} changes...'.format(FEED_ID_CMDS))
+    client.subscribe(FEED_ID_CMDS)
 
 def subscribe(client, userdata, mid, granted_qos):
-    print('Subscribed to {0} with QoS {1}'.format(FEED_ID, granted_qos[0]))
+    print('Subscribed to {0} with QoS {1}'.format(FEED_ID_CMDS, granted_qos[0]))
 
 def disconnected(client):
     print('Disconnected from Adafruit IO!')
@@ -38,4 +42,29 @@ if __name__ == '__main__':
 
     client.connect()
 
-    client.loop_blocking()
+    client.loop_background()
+
+    while True:
+        ser.write(b"READ\n")
+        line = ser.readline().decode('utf-8').rstrip()
+        print(line)
+        datadict = None
+        if (line != ""):
+             try:
+                 datadict = eval(line)
+                 print(datadict)
+             except SyntaxError:
+                 pass
+        if (datadict):
+             if ('Water Temperature (C)' in datadict):
+                 value = datadict['Water Temperature (C)']
+                 client.publish(FEED_ID_WATERTEMP, value)
+             if ('pH' in datadict):
+                 value = datadict['pH']
+                 client.publish(FEED_ID_PH, value)
+             if ('PPM' in datadict):
+                 value = datadict['PPM']
+                 client.publish(FEED_ID_TDS, value)
+        time.sleep(20)
+
+
